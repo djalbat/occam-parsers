ba1d638e3ff7b3b1a9b3fd7f5a361c1966819863
James Smith <jecs@imperial.ac.uk>
2016-10-17 09:55:37 +0100

    Altered architecture around the basic example.

--- examples/es6/basicExample.js
+++ examples/es6/basicExample.js
@@ -1,41 +1,48 @@
 'use strict';
 
-var lexers = require('occam-lexers'),
+var easyui = require('easyui'),
+    lexers = require('occam-lexers'),
+    Input = easyui.Input,
+    BNFLexer = lexers.BNFLexer,
     BasicLexer = lexers.BasicLexer;
 
-var Example = require('./example');
+var Example = require('./example'),
+    Parser = require ('../../es6/parser'),
+    BNFParser = require ('../../es6/bnfParser');
 
-var mappings = {},
-    basicLexer = undefined;
+var lexer = undefined,
+    parser = undefined,
+    terminalSymbolsRegExpPatternInputSelector = 'input#terminalSymbolsRegExpPattern',
+    terminalSymbolsRegExpPatternInput = new Input(terminalSymbolsRegExpPatternInputSelector);
 
 class BasicExample extends Example {
   static run() {
-    updateBasicLexer();
+    updateLexer();
+    updateParser();
 
-    Example.updateParser(mappings);
+    terminalSymbolsRegExpPatternInput.onChange(function() {
+      updateLexer();
+      updateParser();
 
-    Example.terminalSymbolsRegExpPatternInput.onChange(function() {
-      updateBasicLexer();
-
-      Example.updateParser(mappings);
-      Example.updateParseTree(basicLexer);
+      Example.updateParseTree(lexer, parser);
     });
 
     Example.grammarTextArea.onChange(function() {
-      Example.updateParser(mappings);
-      Example.updateParseTree(basicLexer);
+      updateParser();
+
+      Example.updateParseTree(lexer, parser);
     });
 
     Example.contentTextArea.onChange(function() {
-      Example.updateParseTree(basicLexer);
+      Example.updateParseTree(lexer, parser);
     });
   }
 }
 
 module.exports = BasicExample;
 
-function updateBasicLexer() {
-  var terminalSymbolsRegExpPatternInputValue = Example.terminalSymbolsRegExpPatternInput.getValue(),
+function updateLexer() {
+  var terminalSymbolsRegExpPatternInputValue = terminalSymbolsRegExpPatternInput.getValue(),
       terminalSymbolsRegExpPattern = terminalSymbolsRegExpPatternInputValue; ///
 
       try {
@@ -50,5 +57,18 @@ function updateBasicLexer() {
         { terminal : terminalSymbolsRegExp }
       ];
 
-  basicLexer = BasicLexer.fromGrammar(grammar);
+  lexer = BasicLexer.fromGrammar(grammar);
 }
+
+function updateParser() {
+  var grammarTextAreaValue = Example.grammarTextArea.getValue(),
+      terminalSymbolsRegExpPatternInputValue = terminalSymbolsRegExpPatternInput.getValue(),
+      grammar = grammarTextAreaValue, ///
+      lines = BNFLexer.linesFromGrammar(grammar),
+      terminalSymbolsRegExpPattern = terminalSymbolsRegExpPatternInputValue, ///
+      mappings = {},
+      productions = BNFParser.parse(lines, terminalSymbolsRegExpPattern, mappings);
+
+  parser = new Parser(productions);
+}
+
--- examples/es6/example.js
+++ examples/es6/example.js
@@ -2,9 +2,6 @@
 
 var easyui = require('easyui'),
     easyuilayout = require('easyui-layout'),
-    occamLexers = require('occam-lexers'),
-    BNFLexer = occamLexers.BNFLexer,
-    Input = easyui.Input,
     TextArea = easyui.TextArea,
     SizeableElement = easyuilayout.SizeableElement,
     VerticalSplitter = easyuilayout.VerticalSplitter;
@@ -13,34 +10,16 @@ var leftColumnSelector = '#leftColumn',
     contentTextAreaSelector = 'textArea#content',
     grammarTextAreaSelector = 'textArea#grammar',
     parseTreeTextAreaSelector = 'textArea#parseTree',
-    terminalSymbolsRegExpPatternInputSelector = 'input#terminalSymbolsRegExpPattern',
     contentTextArea = new TextArea(contentTextAreaSelector),
     grammarTextArea = new TextArea(grammarTextAreaSelector),
     parseTreeTextArea = new TextArea(parseTreeTextAreaSelector),
-    terminalSymbolsRegExpPatternInput = new Input(terminalSymbolsRegExpPatternInputSelector),
     leftColumn = new SizeableElement(leftColumnSelector),
     TO_THE_RIGHT_OF = VerticalSplitter.situated.TO_THE_RIGHT_OF;
 
 new VerticalSplitter('.left.vertical.splitter', TO_THE_RIGHT_OF, leftColumn);
 
-var Parser = require ('../../es6/parser'),
-    BNFParser = require ('../../es6/bnfParser');
-
-var parser = undefined;
-
 class Example {
-  static updateParser(mappings) {
-    var grammarTextAreaValue = grammarTextArea.getValue(),
-        terminalSymbolsRegExpPatternInputValue = terminalSymbolsRegExpPatternInput.getValue(),
-        grammar = grammarTextAreaValue, ///
-        terminalSymbolsRegExpPattern = terminalSymbolsRegExpPatternInputValue, ///
-        lines = BNFLexer.linesFromGrammar(grammar),
-        productions = BNFParser.parse(lines, terminalSymbolsRegExpPattern, mappings);
-
-    parser = new Parser(productions);
-  }
-
-  static updateParseTree(lexer) {
+  static updateParseTree(lexer, parser) {
     var contentTextAreaValue = contentTextArea.getValue(),
         content = contentTextAreaValue,  ///
         tokens = lexer.tokensFromContent(content),
@@ -55,6 +34,5 @@ class Example {
 
 Example.contentTextArea = contentTextArea;
 Example.grammarTextArea = grammarTextArea;
-Example.terminalSymbolsRegExpPatternInput = terminalSymbolsRegExpPatternInput;
 
 module.exports = Example;
