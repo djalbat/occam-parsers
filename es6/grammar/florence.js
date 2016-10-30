ea58f148a7883d8484a918c8760c6ab5197504c4
James Smith <james.smith@djalbat.com>
2016-10-30 21:34:14 +0000

    Added variables to the Florence grammar.

--- es6/grammar/florence.js
+++ es6/grammar/florence.js
@@ -2,107 +2,93 @@
 
 var grammar = `
 
-    document                        ::=   assertion*
+    document                                  ::=   assertion*
 
-    assertion                       ::=   rule | theorem | lemma
+    assertion                                 ::=   rule | axiom | theorem | lemma | variable(s)
     
     
     
-    theorem                         ::=   theoremDeclaration premise(s)? conclusion proof
+    rule                                      ::=   Rule parenthesisedLabel? <END_OF_LINE>+ premise(s)? conclusion proof?
     
-    theoremDeclaration              ::=   Theorem parenthesisedLabel? <END_OF_LINE>+
+    axiom                                     ::=   Axiom parenthesisedLabel? <END_OF_LINE>+ premise(s)? conclusion
     
-    
-    
-    lemma                           ::=   lemmaDeclaration premise(s)? conclusion proof
-    
-    lemmaDeclaration                ::=   Lemma parenthesisedLabel? <END_OF_LINE>+
+    theorem                                   ::=   Theorem parenthesisedLabel? <END_OF_LINE>+ premise(s)? conclusion proof
+        
+    lemma                                     ::=   Lemma parenthesisedLabel? <END_OF_LINE>+ premise(s)? conclusion proof
     
     
     
-    rule                            ::=   ruleDeclaration premise(s)? conclusion proof?
+    premise(s)                                ::=   premise | premises
     
-    ruleDeclaration                 ::=   Rule parenthesisedLabel? <END_OF_LINE>+
+    premise                                   ::=   Premise <END_OF_LINE>+ (labelled)statement
     
-    
-    
-    premise(s)                      ::=   premise | premises
-    
-    premise                         ::=   premiseDeclaration (labelled)statement
-    
-    premises                        ::=   premisesDeclaration (labelled)statement (labelled)statement+
-    
-    premiseDeclaration              ::=   Premise <END_OF_LINE>+
-    
-    premisesDeclaration             ::=   Premises <END_OF_LINE>+
+    premises                                  ::=   Premises <END_OF_LINE>+ (labelled)statement (labelled)statement+
+
+    conclusion                                ::=   Conclusion <END_OF_LINE>+ statement
+
+    proof                                     ::=   Proof <END_OF_LINE>+ subLemmaOr(labelled)statement* therefore
+
+    therefore                                 ::=   Therefore <END_OF_LINE>+ (labelled)statement
     
     
 
-    conclusion                      ::=   conclusionDeclaration statement
-    
-    conclusionDeclaration           ::=   Conclusion <END_OF_LINE>+
+    subLemmaOr(labelled)statement             ::=   subLemma | (labelled)statement
+
+    subLemma                                  ::=   suppose then? hence
     
     
 
-    proof                           ::=   proofDeclaration subLemmaOr(labelled)statement* therefore
+    suppose                                   ::=   Suppose <END_OF_LINE>+ statement+
     
-    proofDeclaration                ::=   Proof <END_OF_LINE>+
+    then                                      ::=   Then <END_OF_LINE>+ subLemmaOr(labelled)statement+
     
+    hence                                     ::=   Hence <END_OF_LINE>+ (labelled)statement
+    
+    
+    
+    (labelled)statement                       ::=   labelledStatement | statement
+    
+    labelledStatement                         ::=   symbol+ by label <END_OF_LINE>+
+    
+    statement                                 ::=   symbol+ <END_OF_LINE>+
+    
+    symbol                                    ::=   unassigned | { | } | = | : | ( | )
+    
+    
+    
+    parenthesisedLabel                        ::=   (<NO_WHITESPACE>label<NO_WHITESPACE>)
+
+    label                                     ::=   unassigned
     
 
-    therefore                       ::=   thereforeDeclaration (labelled)statement
-    
-    thereforeDeclaration            ::=   Therefore <END_OF_LINE>+
-    
-    
 
-    subLemmaOr(labelled)statement   ::=   subLemma | (labelled)statement
+    variable(s)                               ::=   variable | variables
 
-    subLemma                        ::=   suppose then? hence
+    variables                                 ::=   Variables <END_OF_LINE>+ variable(s)Declaration(s)
     
+    variable                                  ::=   Variable <END_OF_LINE>+ variableDeclaration
     
+    variable(s)Declaration(s)                 ::=   variables(s)Declarations | variablesDeclaration
 
-    suppose                         ::=   supposeDeclaration supposition+
+    variables(s)Declarations                  ::=   variable(s)Declaration semiColonThenVariable(s)Declaration+
+       
+    semiColonThenVariable(s)Declaration       ::=   ; variable(s)Declaration
     
-    supposeDeclaration              ::=   Suppose <END_OF_LINE>+
+    variable(s)Declaration                    ::=   variablesDeclaration | variableDeclaration
     
-    supposition                     ::=   statement
-    
-    then                            ::=   thenDeclaration subLemmaOr(labelled)statement+
-    
-    thenDeclaration                 ::=   Then <END_OF_LINE>+
-    
-    hence                           ::=   henceDeclaration (labelled)statement
-    
-    henceDeclaration                ::=   Hence <END_OF_LINE>+
-    
-    
-    
-    (labelled)statement             ::=   labelledStatement | statement
-    
-    labelledStatement               ::=   symbol+ by label <END_OF_LINE>+
-    
-    statement                       ::=   symbol+ <END_OF_LINE>+
-    
-    symbol                          ::=   unassigned
-                                      
-                                      |   { 
-                                      
-                                      |   } 
-                                      
-                                      |   = 
-                                      
-                                      |   : 
-    
-                                      |   ( 
-                                      
-                                      |   )
-    
-    
-    
-    parenthesisedLabel              ::=   (<NO_WHITESPACE>label<NO_WHITESPACE>)
+    variablesDeclaration                      ::=   variableNames colonThenTypeName?
 
-    label                           ::=   unassigned
+    variableDeclaration                       ::=   variableName colonThenTypeName?
+
+    variableNames                             ::=   variableName commaThenVariableName+
+
+    commaThenVariableName                     ::=   , variableName    
+    
+    colonThenTypeName                         ::=   : typeName
+
+    variableName                              ::=   unassigned
+
+    typeName                                  ::=   unassigned
     
 `;
 
