484237eb6710c79b4df14555206b7bfdb773e52c
James Smith <jecs@imperial.ac.uk>
2017-01-14 17:39:05 +0000

    Exported the error node.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var parsers = {
+  'ErrorNode': require('./lib/bnf/node/error'),
   'BNFParser': require('./lib/bnf/parser'),
   'BasicParser': require('./lib/basic/parser'),
   'GallinaParser': require('./lib/gallina/parser'),
-  'FlorenceParser': require('./lib/florence/parser')
+  'FlorenceParser': require('./lib/florence/parser'),
 };
 
 module.exports = parsers;
-
