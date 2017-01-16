c22cc589d71c733727b4db2e98467bcfb16dd06e
James Smith <jecs@imperial.ac.uk>
2017-01-16 18:17:11 +0000

    Removed reference to the fatal error node from the index file.

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var parsers = {
   'BNFParser': require('./lib/bnf/parser'),
   'BasicParser': require('./lib/basic/parser'),
   'GallinaParser': require('./lib/gallina/parser'),
-  'FlorenceParser': require('./lib/florence/parser'),
-  'FatalErrorNode': require('./lib/bnf/node/fatalError'),
+  'FlorenceParser': require('./lib/florence/parser')
 };
 
 module.exports = parsers;
