10226ea53c5708f6bd7c7b1180d2e95227979437
James Smith <jecs@imperial.ac.uk>
2017-02-12 10:17:31 +0000

    Corrected the exports.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var parsers = {
   'BNFParser': require('./lib/bnf/parser'),
   'BasicParser': require('./lib/basic/parser'),
   'FlorenceParser': require('./lib/florence/parser'),
-  'ErrorsNode': require('./lib/florence/node/errors'),
+  'ErrorsNode': require('./lib/florence/errorsNode'),
   'TerminalNode': require('./lib/common/node/terminal'),
   'NonTerminalNode': require('./lib/common/node/nonTerminal')
 };
