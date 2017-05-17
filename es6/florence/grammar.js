cd9a22eca7cf0033d9af015224cac2f4176887f4
James Smith <jecs@imperial.ac.uk>
2017-05-17 12:59:24 +0100

    Fixed the Florence grammar to allow vertical space in the proofs.

--- es6/florence/grammar.js
+++ es6/florence/grammar.js
@@ -2,88 +2,90 @@
 
 const grammar = `
 
-    document                                  ::=   header? verticalSpace? body?
+    document                                          ::=   header? verticalSpace? body?
                                               
                                               
                                               
-    header                                    ::=   includeDirective+
+    header                                            ::=   includeDirective+
     
-    includeDirective                          ::=   'include'<NO_WHITESPACE>'('<NO_WHITESPACE>[string]<NO_WHITESPACE>')' <END_OF_LINE>
+    includeDirective                                  ::=   'include'<NO_WHITESPACE>'('<NO_WHITESPACE>[string]<NO_WHITESPACE>')' <END_OF_LINE>
                                               
                                               
-    body                                      ::=   part+  
+    body                                              ::=   part+  
         
-    part                                      ::=   rule | axiom | theorem | lemma | verticalSpace | error
+    part                                              ::=   rule | axiom | theorem | lemma | verticalSpace | error
     
     
     
-    rule                                      ::=   'Rule' parenthesisedLabels? <END_OF_LINE> premise(s)? conclusion proof?
+    rule                                              ::=   'Rule' parenthesisedLabels? <END_OF_LINE> premise(s)? conclusion proof?
     
-    axiom                                     ::=   'Axiom' parenthesisedLabels? <END_OF_LINE> premise(s)? conclusion
+    axiom                                             ::=   'Axiom' parenthesisedLabels? <END_OF_LINE> premise(s)? conclusion
     
-    theorem                                   ::=   'Theorem' parenthesisedLabels? <END_OF_LINE> premise(s)? conclusion proof
+    theorem                                           ::=   'Theorem' parenthesisedLabels? <END_OF_LINE> premise(s)? conclusion proof
         
-    lemma                                     ::=   'Lemma' parenthesisedLabels? <END_OF_LINE> premise(s)? conclusion proof    
+    lemma                                             ::=   'Lemma' parenthesisedLabels? <END_OF_LINE> premise(s)? conclusion proof    
     
     
     
-    parenthesisedLabels                       ::=   '('<NO_WHITESPACE>labels<NO_WHITESPACE>')'
+    parenthesisedLabels                               ::=   '('<NO_WHITESPACE>labels<NO_WHITESPACE>')'
     
-    labels                                    ::=   label<NO_WHITESPACE>commaThenLabel*
+    labels                                            ::=   label<NO_WHITESPACE>commaThenLabel*
     
-    commaThenLabel                            ::=   ','<NO_WHITESPACE>label
-    
-    
-    
-    premise(s)                                ::=   premise | premises
-    
-    premise                                   ::=   'Premise' <END_OF_LINE> (un)labelledStatement
-    
-    premises                                  ::=   'Premises' <END_OF_LINE> (un)labelledStatement (un)labelledStatement+
-    
-    conclusion                                ::=   'Conclusion' <END_OF_LINE> (un)labelledStatement
-    
-    proof                                     ::=   'Proof' <END_OF_LINE> subLemmaOr(un)labelledStatement* therefore
-    
-    therefore                                 ::=   'Therefore' <END_OF_LINE> (un)labelledStatement
+    commaThenLabel                                    ::=   ','<NO_WHITESPACE>label
     
     
     
-    subLemmaOr(un)labelledStatement           ::=   subLemma | (un)labelledStatement
+    premise(s)                                        ::=   premise | premises
     
-    subLemma                                  ::=   suppose then? hence verticalSpace?
+    premise                                           ::=   'Premise' <END_OF_LINE> (un)labelledStatement
     
+    premises                                          ::=   'Premises' <END_OF_LINE> (un)labelledStatement (un)labelledStatement+
     
+    conclusion                                        ::=   'Conclusion' <END_OF_LINE> (un)labelledStatement
     
-    suppose                                   ::=   'Suppose' <END_OF_LINE> unlabelledStatement+
+    proof                                             ::=   'Proof' <END_OF_LINE> subLemmaOr(un)labelledStatementThenVerticalSpace* therefore
     
-    then                                      ::=   'Then' <END_OF_LINE> subLemmaOr(un)labelledStatement+
-    
-    hence                                     ::=   'Hence' <END_OF_LINE> (un)labelledStatement
-    
-    
-    
-    (un)labelledStatement                     ::=   unlabelledStatement | labelledStatement
-    
-    labelledStatement                         ::=   statement 'by' label <END_OF_LINE>  
-    
-    unlabelledStatement                       ::=   statement <END_OF_LINE>  
-    
-    statement                                 ::=   specialOrUnassigned+ 
-    
-    specialOrUnassigned                       ::=   [special] | [unassigned]
+    therefore                                         ::=   'Therefore' <END_OF_LINE> (un)labelledStatement
     
     
     
-    label                                     ::=   [unassigned]
+    subLemmaOr(un)labelledStatementThenVerticalSpace  ::=   subLemmaOr(un)labelledStatement verticalSpace?   
+    
+    subLemmaOr(un)labelledStatement                   ::=   subLemma | (un)labelledStatement
+    
+    subLemma                                          ::=   suppose then? hence
+    
+    
+    
+    suppose                                           ::=   'Suppose' <END_OF_LINE> unlabelledStatement+
+    
+    then                                              ::=   'Then' <END_OF_LINE> subLemmaOr(un)labelledStatement+
+    
+    hence                                             ::=   'Hence' <END_OF_LINE> (un)labelledStatement
+    
+    
+    
+    (un)labelledStatement                             ::=   unlabelledStatement | labelledStatement
+    
+    labelledStatement                                 ::=   statement 'by' label <END_OF_LINE>  
+    
+    unlabelledStatement                               ::=   statement <END_OF_LINE>  
+    
+    statement                                         ::=   specialOrUnassigned+ 
+    
+    specialOrUnassigned                               ::=   [special] | [unassigned]
+    
+    
+    
+    label                                             ::=   [unassigned]
      
      
      
-    verticalSpace                             ::=   <END_OF_LINE>+
+    verticalSpace                                     ::=   <END_OF_LINE>+
     
     
     
-    error                                     ::=   *
+    error                                             ::=   *
     
 `;
 
