74d6f6441d0befb4776fb4883667613e192280d1
James Smith <jecs@imperial.ac.uk>
2017-05-28 13:04:00 +0100

    Removed queries.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var parsers = {
-  'Query': require('./lib/query'),
   'BNFParser': require('./lib/bnf/parser'),
   'BasicParser': require('./lib/basic/parser'),
   'FlorenceParser': require('./lib/florence/parser'),
