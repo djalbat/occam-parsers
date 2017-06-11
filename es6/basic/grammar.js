e84e5ae9df554ee01bae2ab411fd85272e8f7350
James Smith <jecs@imperial.ac.uk>
2017-06-11 18:44:07 +0100

    Reset the basic example grammar.

--- es6/basic/grammar.js
+++ es6/basic/grammar.js
@@ -2,67 +2,12 @@
 
 const grammar = `
 
- A ::= A 'a' | B 'b' | 'd'
-
- B ::= C 'c'
-
- C ::= A 'e' | B 'f'
-
-
-`;
-
-module.exports = grammar;
-
-/*
-
  expression               ::=  expression '+' term | term
 
  term                     ::=  naturalNumber
 
  naturalNumber            ::=  /\\d+/
 
+`;
 
-
- A ::= B | 'a'
-
- B ::= C | 'b' 'c'
-
- C ::= 'd' 'e' | A
-
-
-
- A ::= B | 'a'
-
- B ::= C 'b' | 'c'
-
- C ::= d | A 'e'
-
-
-
- S ::= X 'b' | Y
-
- Y ::= X | 'a'
-
- X ::= S | 'c'
-
-
-
- A1 ::= A2 'a' | 'b'
-
- A2 ::= A1 'c' | 'd'
-
-
-
-  expression               ::=  term operatorThenTerm*
-  
-  operatorThenTerm         ::=  operator term
-  
-  operator                 ::=  '+' | '-' | '*' | '/'
-  
-  term                     ::=  naturalNumber | parenthesizedExpression
-  
-  naturalNumber            ::=  /\\d+/
-  
-  parenthesizedExpression  ::=  '(' expression ')'
-
-*/
+module.exports = grammar;
