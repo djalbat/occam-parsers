583b37da8a8d1f2a15120fee6fc7107ad7ea2088
James Smith <jecs@imperial.ac.uk>
2017-06-24 15:09:19 +0100

    Fixed the index file.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var parsers = {
-  'BNFParser': require('./lib/bnf/parser'),
   'BasicParser': require('./lib/basic/parser'),
   'FlorenceParser': require('./lib/florence/parser'),
+  'ExtendedBNFParser': require('./lib/extendedBNF/parser'),
   'TerminalNode': require('./lib/common/node/terminal'),
   'NonTerminalNode': require('./lib/common/node/nonTerminal'),
   'ErrorNode': require('./lib/common/node/terminal/error')
