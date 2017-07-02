7c62865da4a5053cac05e1f0720c36307127507d
James Smith <jecs@imperial.ac.uk>
2017-07-02 12:52:24 +0100

    Fixed the index file and a little SASS.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var parsers = {
-  'cycles': require('./lib/grammar/cycles'),
-  'leftRecursion': require('./lib/grammar/leftRecursion'),
+  'BNFParser': require('./lib/bnf/parser'),
   'BasicParser': require('./lib/basic/parser'),
   'FlorenceParser': require('./lib/florence/parser'),
-  'ExtendedBNFParser': require('./lib/extendedBNF/parser'),
   'TerminalNode': require('./lib/common/node/terminal'),
   'NonTerminalNode': require('./lib/common/node/nonTerminal'),
-  'ErrorNode': require('./lib/common/node/terminal/error')
+  'ErrorNode': require('./lib/common/node/terminal/error'),
+  'cycles': require('./lib/grammar/cycles'),
+  'leftRecursion': require('./lib/grammar/leftRecursion')
 };
 
 module.exports = parsers;
