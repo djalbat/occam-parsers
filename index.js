c57e747c7c89ef7ed693be56059ca1b26c865cd1
James Smith <jecs@imperial.ac.uk>
2017-07-02 13:28:44 +0100

    Exported the necessasry.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 'use strict';
 
 var parsers = {
+  'Rule': require('./lib/bnf/rule'),
+  'Definition': require('./lib/bnf/definition'),
+  'EpsilonPart': require('./lib/bnf/part/epsilon'),
+  'RuleNamePart': require('./lib/bnf/part/ruleName'),
   'BNFParser': require('./lib/bnf/parser'),
   'BasicParser': require('./lib/basic/parser'),
   'FlorenceParser': require('./lib/florence/parser'),
   'TerminalNode': require('./lib/common/node/terminal'),
   'NonTerminalNode': require('./lib/common/node/nonTerminal'),
-  'ErrorNode': require('./lib/common/node/terminal/error'),
-  'cycles': require('./lib/grammar/cycles'),
-  'leftRecursion': require('./lib/grammar/leftRecursion')
+  'ErrorNode': require('./lib/common/node/terminal/error')
 };
 
 module.exports = parsers;
