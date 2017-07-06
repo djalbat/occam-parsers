4e7c21355576ff10121922b8ff84402c2a4519a2
James Smith <jecs@imperial.ac.uk>
2017-07-06 17:53:41 +0100

    Added a plain parser.

---
+++ es6/plain/parser.js
@@ -0,0 +1,17 @@
+'use strict';
+
+class PlainParser {
+  nodeFromLines(lines) {
+    const node = null;  ///
+    
+    return node;
+  }
+  
+  static fromNothing() {
+    const plainParser = new PlainParser();
+
+    return plainParser;
+  }
+}
+
+module.exports = PlainParser;
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var parsers = {
   'BNFParser': require('./lib/bnf/parser'),
   'BasicParser': require('./lib/basic/parser'),
   'FlorenceParser': require('./lib/florence/parser'),
+  'PlainParser': require('./lib/plain/parser'),
   'TerminalNode': require('./lib/common/node/terminal'),
   'NonTerminalNode': require('./lib/common/node/nonTerminal'),
   'ErrorNode': require('./lib/common/node/terminal/error')
