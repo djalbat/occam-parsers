03065dd1b5a7f40630191de459734b007533207e
James Smith <jecs@imperial.ac.uk>
2017-07-09 14:37:15 +0100

    Small changes to the index file.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var parsers = {
   'BNFParser': require('./lib/bnf/parser'),
   'BasicParser': require('./lib/basic/parser'),
   'FlorenceParser': require('./lib/florence/parser'),
+  'CommonParser': require('./lib/common/parser'),
   'TerminalNode': require('./lib/common/node/terminal'),
   'NonTerminalNode': require('./lib/common/node/nonTerminal'),
   'ErrorNode': require('./lib/common/node/terminal/error')
