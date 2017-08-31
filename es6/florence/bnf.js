c24833c2ef731f9e1d0103eb3c26e603ae55cbe5
James Smith <jecs@imperial.ac.uk>
2017-08-31 16:07:33 +0100

    Corrected the definition of nonsense.

--- es6/florence/bnf.js
+++ es6/florence/bnf.js
@@ -207,7 +207,7 @@ const bnf = `
      
      
 
-     nonsense                             ::=   ( [unassigned] | [special] )+ ;
+     nonsense                             ::=   ( [unassigned] | [special] | [custom]  )+ ;
 
      
 `;
