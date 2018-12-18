4f83b5dcb28ed4d4e4fb64e189552b49221972ed
James Smith <james.smith@openmathematics.org>
2018-12-18 14:48:48 +0000

    Put back the meta JSON parser.

---
+++ es6/metaJSON/bnf.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const bnf = `
+
+
+    document      ::= metaJSON | error+ ;
+
+
+    metaJSON      ::= "{" (
+     
+                           ( repository ( "," dependencies )? ) 
+                           
+                         | ( ( dependencies "," )? repository )
+                            
+                      ) "}" ;
+
+
+    repository    ::= "\\"repository\\"" ":" [stringLiteral] ;
+
+
+    dependencies  ::= "\\"dependencies\\"" ":" "[" (dependency ( "," dependency )*)?  "]" ;
+
+
+    dependency    ::= [stringLiteral] ;
+
+
+    error         ::= . ;
+
+
+`;
+
+module.exports = bnf;
---
+++ es6/metaJSON/parser.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const lexers = require('occam-lexers');
+
+const bnf = require('./bnf'),
+      BNFParser = require('../bnf/parser'),
+      CommonParser = require('../common/parser');
+
+const { BNFLexer } = lexers;
+
+const bnfLexer = BNFLexer.fromNothing(),
+      bnfParser = BNFParser.fromNothing();
+
+class MetaJSONParser extends CommonParser {
+  static fromBNF(bnf) {
+    const tokens = bnfLexer.tokensFromBNF(bnf),
+          rulesNode = bnfParser.rulesNodeFromTokens(tokens),
+          rules = BNFParser.generateRules(rulesNode),
+          metaJSONParser = new MetaJSONParser(rules);
+
+    return metaJSONParser;
+  }
+
+  static fromNothing() {
+    const metaJSONParser = MetaJSONParser.fromBNF(bnf);
+
+    return metaJSONParser;
+  }
+}
+
+Object.assign(MetaJSONParser, {
+  bnf
+});
+
+module.exports = MetaJSONParser;
