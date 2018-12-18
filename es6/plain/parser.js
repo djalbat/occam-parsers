523f9e3f89bccb54edb088f684f8f5bd48e7c9e0
James Smith <james.smith@openmathematics.org>
2018-12-18 14:46:45 +0000

    Put back the plain parser.

---
+++ es6/plain/parser.js
@@ -0,0 +1,17 @@
+'use strict';
+
+class PlainParser {
+  parse(tokens) {
+    const node = null;  ///
+
+    return node;
+  }
+
+  static fromNothing() {
+    const plainParser = new PlainParser();
+
+    return plainParser;
+  }
+}
+
+module.exports = PlainParser;
