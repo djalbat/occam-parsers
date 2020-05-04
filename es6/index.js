da09919837c52e884fda5a9df28e4a60d527e142
James Smith <james.smith@openmathematics.org>
2020-05-04 10:15:09 +0100

    Added the index file.

---
+++ es6/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+export { default as Rule } from "./bnf/rule";
+export { default as Parts } from "./bnf/parts";
+export { default as partTypes } from "./bnf/partTypes";
+export { default as Definition } from "./bnf/definition";
+export { default as TerminalNode } from "./common/node/terminal";
+export { default as NonTerminalNode } from "./common/node/nonTerminal";
+export { default as termDefaultBNF } from "./florence/defaultBNF/term";
+export { default as statementDefaultBNF } from "./florence/defaultBNF/statement";
+export { default as expressionDefaultBNF } from "./florence/defaultBNF/expression";
+export { default as metastatementDefaultBNF } from "./florence/defaultBNF/metastatement";
+export { default as BNFParser } from "./bnf/parser";
+export { default as PlainParser } from "./plain/parser";
+export { default as LaTeXParser } from "./latex/parser";
+export { default as BasicParser } from "./basic/parser";
+export { default as CommonParser } from "./common/parser";
+export { default as MetaJSONParser } from "./metaJSON/parser";
+export { default as FlorenceParser } from "./florence/parser";
+export { default as CustomGrammarBNFParser } from "./customGrammarBNF/parser";
+export { default as CustomGrammarLexicalPatternParser } from "./customGrammarLexicalPattern/parser";
