e6a7ba19fddb5eaf3c6261ff3567b5ffd2b6f8c1
James Smith <james.smith@djalbat.com>
2023-06-18 19:46:23 +0100

    Gotten the precedence BNF node working.

--- src/node/bnf/definition.js
+++ src/node/bnf/definition.js
@@ -3,20 +3,23 @@
 import Definition from "../../definition";
 import NonTerminalNode from "../../node/nonTerminal";
 
-import { partRuleName } from "../../ruleNames";
-import { nodesFromChildNodesAndRuleName } from "../../utilities/node";
+import { partRuleName, precedenceRuleName } from "../../ruleNames";
+import { nodeFromChildNodesAndRuleName, nodesFromChildNodesAndRuleName } from "../../utilities/node";
 
 export default class DefinitionBNFNode extends NonTerminalNode {
   generateDefinition() {
     const childNodes = this.getChildNodes(),
           partBNFNodes = nodesFromChildNodesAndRuleName(childNodes, partRuleName),
+          precedenceBNFNode = nodeFromChildNodesAndRuleName(childNodes, precedenceRuleName),
           lookAhead = false,  ///
           parts = partBNFNodes.map((partBNFNode) => {
             const part = partBNFNode.generatePart(lookAhead);
   
             return part;
           }),
-          precedence = null,
+          precedence = (precedenceBNFNode === null) ?
+                          null :
+                            precedenceBNFNode.getPrecedence(),
           definition = Definition.fromPartsAndPrecedence(parts, precedence);
 
     return definition;
--- src/node/bnf/precedence.js
+++ src/node/bnf/precedence.js
@@ -1,7 +1,21 @@
 "use strict";
 
+import { arrayUtilities } from "necessary";
+
 import NonTerminalNode from "../../node/nonTerminal";
 
+const { third } = arrayUtilities;
+
 export default class PrecedenceBNFNode extends NonTerminalNode {
+  getPrecedence() {
+    const childNodes = this.getChildNodes(),
+          thirdChildNode = third(childNodes),
+          terminalNode = thirdChildNode,  ///
+          content = terminalNode.getContent(),
+          precedence = Number(content); ///
+
+    return precedence;
+  }
+
   static fromRuleNamePrecedenceAndChildNodes(ruleName, precedence, childNodes) { return NonTerminalNode.fromRuleNamePrecedenceAndChildNodes(PrecedenceBNFNode, ruleName, precedence, childNodes); }
 }
