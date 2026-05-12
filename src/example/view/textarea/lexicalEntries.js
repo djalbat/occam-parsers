103944900b1da340b90569fc354a9d53bbed48b6
James Smith <james.smith@djalbat.com>
2026-05-12 21:09:43 +0100

    A tiny improvement.

--- src/example/view/textarea/lexicalEntries.js
+++ src/example/view/textarea/lexicalEntries.js
@@ -7,13 +7,17 @@ import Textarea from "../textarea";
 class LexicalEntriesTextarea extends Textarea {
   getLexicalEntries() {
     const value = this.getValue(),
-          lexicalEntries = JSON.parse(value);
+          jsonString = value, ///
+          json = JSON.parse(jsonString),
+          lexicalEntries = json;  ///
 
     return lexicalEntries;
   }
 
   setLexicalEntries(lexicalEntries) {
-    const value = JSON.stringify(lexicalEntries, null, 2);
+    const json = lexicalEntries,
+          jsonString = JSON.stringify(json, null, 2),
+          value = jsonString; ///
 
     this.setValue(value);
   }
