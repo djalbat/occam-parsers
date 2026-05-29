745e89a39bc5f8466a262d00b10ef628f0fba1b7
James Smith <james.smith@djalbat.com>
2026-05-29 14:40:44 +0100

    Propagated.

--- src/example/view/basic.js
+++ src/example/view/basic.js
@@ -5,19 +5,11 @@ import { BasicParser } from "../../index";  ///
 
 import View from "../view";
 
-import { rulesAsString, rulesFromStartRuleAndRuleMap } from "../../utilities/rules";
-
 export default class BasicView extends View {
   getNode(tokens) {
     const bnf = this.getBNF(),
           basicParser = BasicParser.fromBNF(bnf),
-          node = basicParser.parse(tokens),
-          ruleMap = basicParser.getRuleMap(),
-          startRule = basicParser.getStartRule(),
-          rules = rulesFromStartRuleAndRuleMap(startRule, ruleMap),
-          string  = rulesAsString(rules);
-
-    console.log(string)
+          node = basicParser.parse(tokens);
 
     return node;
   }
